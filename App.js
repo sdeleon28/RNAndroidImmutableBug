7938643df57a8e4756ec16fa17bf8b60a166ddd8
Santiago de Leon <sdeleon28@gmail.com>
2018-03-16 19:17:18 -0300

    Implemented app to test the weird immutable behavior

--- App.js
+++ App.js
@@ -1,23 +1,79 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import { fromJS } from 'immutable';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+const Wrapper = styled.View`
+    flex: 1;
+    align-self: stretch;
+`;
+
+const Item = styled.Text`
+    flex: 1;
+    align-self: stretch;
+    background-color: aquamarine;
+    color: black;
+    font-size: 25;
+`;
+
+const Button = styled.TouchableOpacity`
+    flex: 1;
+    background-color: blue;
+`;
+
+const ButtonLabel = styled.Text``;
+
+const initialState = fromJS({
+    items: [
+        {
+            text: 'Page 1 - Item 0',
+            position: 0
+        },
+        {
+            text: 'Page 1 - Item 1',
+            position: 1
+        },
+        {
+            text: 'Page 1 - Item 2',
+            position: 2
+        }
+    ]
+});
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            appState: initialState
+        };
+    }
+
+    deleteItem1 = () => {
+        const newState = this.state.appState.deleteIn(['items', 1]);
+
+        // Uncomment the following block to log the problematic state
+
+        // console.log('==================================================');
+        // console.log(`
+        //     newState
+        //     --------------------------------------------------
+        //     ${JSON.stringify(newState.toJS())}
+        // `);
+        // console.log('==================================================');
+
+        this.setState({ appState: newState });
+    };
+
+    render() {
+        const { appState } = this.state;
+        return (
+            <Wrapper>
+                {appState.get('items').map(item => <Item key={item.get('position')}>{item.get('text')}</Item>)}
+                <Button onPress={this.deleteItem1}>
+                    <ButtonLabel>Delete item 1</ButtonLabel>
+                </Button>
+            </Wrapper>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
